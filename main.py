a77d95dc82e5f008a603e239a93b93b9cd32b0de
trev <tjohnstone@designfusion.com>
2025-11-20 23:27:14 -0500

    G: Got the player displayed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -11,15 +12,19 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        print(dt)
+
 
 if __name__ == "__main__":
     main()
