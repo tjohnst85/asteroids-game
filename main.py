527d3dc9ca658c362be2b76ec8a5176c9cdf3105
trev <tjohnstone@designfusion.com>
2025-11-22 21:37:54 -0500

    J:Added groups to help manage what is updated and drawn better

--- main.py
+++ main.py
@@ -12,7 +12,12 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable =  pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
 
 
     while True:
@@ -21,10 +26,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        
+        for drawings in drawable:
+            drawings.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        player.update(dt)
+        updatable.update(dt)
 
 if __name__ == "__main__":
     main()
